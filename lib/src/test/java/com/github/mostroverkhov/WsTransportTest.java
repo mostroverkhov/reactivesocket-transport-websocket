ce6e7c915ec2008f0f0dbd3ac851f18b0cf54b82
maksim <M.Ostroverkhov@gmail.com>
2016-11-29 13:00:27 +0200

    remove unrelated test code

--- lib/src/test/java/com/github/mostroverkhov/WsTransportTest.java
+++ lib/src/test/java/com/github/mostroverkhov/WsTransportTest.java
@@ -18,34 +18,7 @@ public class WsTransportTest {
 
     @Ignore
     @Test
-    public void ws() throws Exception {
-
-        HttpServer<ByteBuf, ByteBuf> server;
-
-        /*Starts a new HTTP server on an ephemeral port.*/
-        server = HttpServer.newServer()
-                .enableWireLogging("msg-server", LogLevel.DEBUG)
-                .start((req, resp) -> {
-                               /*If WebSocket upgrade is requested, then accept the request with an echo handler.*/
-                    if (req.isWebSocketUpgradeRequested()) {
-                        return resp.acceptWebSocketUpgrade(wsConn -> {
-                            Observable<WebSocketFrame> in = wsConn.getInput()
-                                    .filter(wsf -> wsf instanceof BinaryWebSocketFrame)
-                                    .cast(BinaryWebSocketFrame.class)
-                                    .map(f -> {
-                                        ByteBuf data = f.content();
-                                        data.setByte(data.readerIndex(), 1);
-                                        return new MessageFrame(data);
-                                    });
-                            return wsConn.writeAndFlushOnEach(in);
-                        });
-                    } else {
-                                   /*Else send a NOT FOUND response.*/
-                        return resp.setStatus(HttpResponseStatus.NOT_FOUND);
-                    }
-                });
-
-        server.awaitShutdown();
+    public void clientServerSmokeTest() throws Exception {
 
     }
 }
